d86976ddcd3a04d408b59e43604c493f5acd5091
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2022-10-30 18:35:21 +0530

    Add files via upload

---
+++ GuiOne.java
@@ -0,0 +1,36 @@
+package mygui;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class GuiOne extends PApplet{
+	PImage img;
+	public void setup() {
+		size(400,400);
+		background(255);
+		stroke(0);
+		img = loadImage("palmTrees.jpg");
+	    img.resize(0,height);
+		image(img,0,0);
+		StringBuffer[] s = {"bindu"  };
+		printArray(s);
+	}
+	public void draw() {
+		int[] color = sunColorSec(second());
+		fill(color[0],color[1],color[2]);
+	    ellipse(width/4,height/5,width/4,height/5);
+	    
+	}
+	public int[] sunColorSec(float seconds)
+	{
+	   int[] rgb= new int[3];
+	   float diffFrom30 =Math.abs(30-seconds);
+	   float ratio = diffFrom30/30;
+	   rgb[0]=(int)(255*ratio);
+	   rgb[1]=(int)(255*ratio);
+	   rgb[2]=0;
+	   
+	   System.out.println("R" + rgb[0] +"G" +rgb[1] + "B" + rgb[2]);
+	   return rgb;
+	}
+
+}
