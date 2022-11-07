af7ed269650ba553932d8c5bae11af8f0f4427f2
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2022-11-07 19:20:45 +0530

    Add files via upload

---
+++ src/module3/EarthquakeCityMap.java
@@ -0,0 +1,135 @@
+package module3;
+
+//Java utilities libraries
+import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.Comparator;
+import java.util.List;
+
+//Processing library
+import processing.core.PApplet;
+
+//Unfolding libraries
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+
+//Parsing library
+import parsing.ParseFeed;
+import processing.core.*;
+/** EarthquakeCityMap
+ * An application with an interactive map displaying earthquake data.
+ * Author: UC San Diego Intermediate Software Development MOOC team
+ * @author Your name here
+ * Date: July 17, 2015
+ * */
+public class EarthquakeCityMap extends PApplet {
+
+	// You can ignore this.  It's to keep eclipse from generating a warning.
+	private static final long serialVersionUID = 1L;
+
+	// IF YOU ARE WORKING OFFLINE, change the value of this variable to true
+	private static final boolean offline = true;
+	
+	// Less than this threshold is a light earthquake
+	public static final float THRESHOLD_MODERATE = 5;
+	// Less than this threshold is a minor earthquake
+	public static final float THRESHOLD_LIGHT = 4;
+
+	/** This is where to find the local tiles, for working without an Internet connection */
+	public static String mbTilesString = "blankLight-1-3.mbtiles";
+	
+	// The map
+private UnfoldingMap map;
+	
+	//feed with magnitude 2.5+ Earthquakes
+	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
+
+	
+	public void setup() {
+		size(1200, 900);
+
+		if (offline) {
+		    map = new UnfoldingMap(this, 1200, 300, 1500, 700, new MBTilesMapProvider(mbTilesString));
+		    earthquakesURL = "2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
+		}
+		else {
+			map = new UnfoldingMap(this, 1200, 300, 900, 700, new Google.GoogleMapProvider());
+			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
+			//earthquakesURL = "2.5_week.atom";
+		}
+		
+			
+	    // The List you will populate with new SimplePointMarkers
+	    List<Marker> markers = new ArrayList<Marker>();
+
+	    //Use provided parser to collect properties for each earthquake
+	    //PointFeatures have a getLocation method
+	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+	    
+	    // These print statements show you (1) all of the relevant properties 
+	    // in the features, and (2) how to get one property and use it
+	    if (earthquakes.size() > 0) {
+	    	PointFeature f = earthquakes.get(0);
+	    	System.out.println(f.getProperties());
+	    	Object magObj = f.getProperty("magnitude");
+	    	float mag = Float.parseFloat(magObj.toString());
+	    	// PointFeatures also have a getLocation method
+	    }
+	    
+	    // Here is an example of how to use Processing's color method to generate 
+	    // an int that represents the color yellow.  
+	    int yellow = color(255, 255, 0);
+	    
+	    //TODO: Add code here as appropriate
+	}
+		
+	// A suggested helper method that takes in an earthquake feature and 
+	// returns a SimplePointMarker for that earthquake
+	// TODO: Implement this method and call it from setUp, if it helps
+	private SimplePointMarker createMarker(PointFeature feature)
+	{
+		// finish implementing and use this method, if it helps.
+		return new SimplePointMarker(feature.getLocation());
+	}
+	
+	public void draw() {
+	    background(10);
+	    map.draw();
+	    addKey();
+	}
+
+
+	// helper method to draw key in GUI
+	// TODO: Implement this method to draw the key
+	private void addKey() 
+	{	
+		fill(255, 250, 240);
+		rect(25, 50, 150, 250);
+		
+		fill(0);
+		textAlign(LEFT, CENTER);
+		textSize(12);
+		text("Earthquake Key", 50, 75);
+		
+		fill(color(255, 0, 0));
+		ellipse(50, 125, 15, 15);
+		fill(color(255, 255, 0));
+		ellipse(50, 175, 10, 10);
+		fill(color(0, 0, 255));
+		ellipse(50, 225, 5, 5);
+		
+		fill(0, 0, 0);
+		text("5.0+ Magnitude", 75, 125);
+		text("4.0+ Magnitude", 75, 175);
+		text("Below 4.0", 75, 225);
+		
+		
+	
+	}
+
+}
